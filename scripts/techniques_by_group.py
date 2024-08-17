0f7eefbabaa0a7003a89e97b0bdb5b1cc2cc0efc
Muralidhar Balcha <murali.balcha@triliodata.com>
2024-08-16 22:12:32 -0400

    Added tactic as argument

--- scripts/techniques_by_group.py
+++ scripts/techniques_by_group.py
@@ -1,7 +1,26 @@
+import argparse
 import os
 import json
 import pandas as pd
+import sys
 
+
+parser = argparse.ArgumentParser(
+                    prog='techniques_by_group',
+                    description='List all techniques associated with a tactic')
+
+parser.add_argument('tactic', choices= ['Reconnaissance', 'Resource Development', 'Initial Access',
+                                        'Execution', 'Persistence', 'Privilege Escalation',
+                                        'Defense Evasion', 'Credential Access, Discovery',
+                                        'Lateral Movement', 'Collection', 'Command and Control',
+                                        'Exfiltration', 'Impact',])
+
+parser.add_argument('-g', '--group')
+
+args = parser.parse_args()
+print(args.tactic, args.group)
+
+tactic = args.tactic.lower()
 techniques_by_tactics = {}
 with open("techniques_by_tactics.json") as f:
     for k, v in json.load(f).items():
@@ -14,7 +33,7 @@ grps = pd.read_csv("groups.csv").sort_values("Group Name")
 for grpname in grps["Group Name"].unique():
     grp_techniques = grps[grps["Group Name"] == grpname]
     for k, v in techniques_by_tactics.items():
-        if k != "discovery":
+        if k != tactic:
             continue
         print("============== %s ===============" % grpname)
         print("%s:" %k)
